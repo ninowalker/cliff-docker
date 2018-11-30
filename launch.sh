65bec9ebc4f5ed3b01044691960eac5effa1f83a
Rahul B <rahulbot@gmail.com>
2018-11-30 10:20:49 -0500

    More work on container deployment

--- launch.sh
+++ launch.sh
@@ -3,31 +3,35 @@ rm tomcat-users.xml
 wget https://raw.githubusercontent.com/ahalterman/CLIFF-up/master/tomcat-users.xml
 $CATALINA_HOME/bin/startup.sh
 
-echo "Downloading CLIFF..."
-cd; wget https://github.com/c4fcm/CLIFF/releases/download/v2.4.0/CLIFF-2.4.0.war
-mv CLIFF-2.4.0.war /usr/local/tomcat/webapps/
+echo "Getting CLIFF..."
+echo "  downloading Cliff WAR file from GitHub"
+cd; wget https://github.com/mitmedialab/CLIFF/releases/download/v2.4.1/cliff-2.4.1.war
+mv cliff-2.4.1.war /usr/local/tomcat/webapps/
+echo "  done (copied /usr/local/tomcat/webapps/)"
 
-echo "Downloading CLAVIN..."
+echo "Building Index..."
+echo "  downloading CLAVIN from GitHub"
 cd; git clone https://github.com/Berico-Technologies/CLAVIN.git
 
-echo "Downloading placenames file from Geonames..."
+echo "  downloading latestplacenames file from Geonames"
 cd CLAVIN
 wget http://download.geonames.org/export/dump/allCountries.zip
 unzip allCountries.zip
 rm allCountries.zip
 
-echo "Compiling CLAVIN"
+echo "  compiling CLAVIN"
 mvn compile
 
-echo "Building Lucene index of placenames -- this is the slow one"
+echo "  building Lucene index of placenames (takes up to 20 mins)"
 MAVEN_OPTS="-Xmx4g" mvn exec:java -Dexec.mainClass="com.bericotech.clavin.index.IndexDirectoryBuilder"
 
-echo "Symlink /etc/cliff2 to the new IndexDirectory that was just built"
+echo "  symlink /etc/cliff2 to the new IndexDirectory that was just built"
 mkdir /etc/cliff2
 cp -r IndexDirectory /etc/cliff2/IndexDirectory
+echo " done"
 
-echo "Start up Tomcat"
+echo "Start up Tomcat..."
 $CATALINA_HOME/bin/shutdown.sh
 $CATALINA_HOME/bin/catalina.sh run
-
-echo "Done! You should try hitting a URL now to see if it works!"
+echo "  done - you should try hitting a URL now to see if it works!"
+echo "  try: http://server.srv/cliff-2.4.1/parse/text?q=This%20is%20about%20India%20the%20country"
