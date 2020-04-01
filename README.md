816688407501b13006ceccf31f35adb92ec888fa
Rahul B <rahulbot@gmail.com>
2020-04-01 15:41:49 -0400

    Update to CLIFF release v2.6.1

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ Building the Image
 Build using
 
 ```
-docker build -t cliff:2.6.0 .
+docker build -t cliff:2.6.1 .
 ```
 
 Running the Image
@@ -21,13 +21,13 @@ Running the Image
 It is setup to use the default port of `8080`. Run using
 
 ```
-docker run -p "8080:8080" -d cliff:2.6.0
+docker run -p "8080:8080" -d cliff:2.6.1
 ```
 
 You can test it at a URL like:
 
 ```
-http://localhost:8080/cliff-2.6.0/parse/text?q=This%20is%20some%20text%20about%20New%20York%20City,%20and%20maybe%20about%20Accra%20as%20well,%20and%20maybe%20Boston%20as%20well.
+http://localhost:8080/cliff-2.6.1/parse/text?q=This%20is%20some%20text%20about%20New%20York%20City,%20and%20maybe%20about%20Accra%20as%20well,%20and%20maybe%20Boston%20as%20well.
 ```
 
 Acknowledgements
--- launch.sh
+++ launch.sh
@@ -5,8 +5,8 @@ $CATALINA_HOME/bin/startup.sh
 
 echo "Getting CLIFF..."
 echo "  downloading Cliff WAR file from GitHub"
-cd; wget https://github.com/mitmedialab/CLIFF/releases/download/v2.6.0/cliff-2.6.0.war
-mv cliff-2.6.0.war /usr/local/tomcat/webapps/
+cd; wget https://github.com/mitmedialab/CLIFF/releases/download/v2.6.1/cliff-2.6.1.war
+mv cliff-2.6.1.war /usr/local/tomcat/webapps/
 echo "  done (copied /usr/local/tomcat/webapps/)"
 
 #echo "Building Index..."
@@ -36,4 +36,4 @@ echo "Start up Tomcat..."
 $CATALINA_HOME/bin/shutdown.sh
 $CATALINA_HOME/bin/catalina.sh run
 echo "  done - you should try hitting a URL now to see if it works!"
-echo "  try: http://localhost:8080/cliff-2.6.0/parse/text?q=This%20is%20about%20India%20the%20country"
+echo "  try: http://localhost:8080/cliff-2.6.1/parse/text?q=This%20is%20about%20India%20the%20country"
